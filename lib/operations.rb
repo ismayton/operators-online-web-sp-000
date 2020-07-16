e6c2f64734b004b90fe5268c7d68ca24fd33bd3b
Ian Mayton <ian.mayton@gmail.com>
2020-07-16 15:11:18 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
 if speed > 90
-  return FALSE
+  return false
+else
+  return true
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed < 90
+	  return true
+	else
+	  return false
 end
 	
 
